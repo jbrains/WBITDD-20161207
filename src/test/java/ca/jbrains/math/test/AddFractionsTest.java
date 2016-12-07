7f12c3cc194a150bc56919e5c28f412a7b07cb30
J. B. Rainsberger <me@jbrains.ca>
2016-12-07 13:48:30 +0100

    Removed obsolete field.

--- src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -45,16 +45,13 @@ public class AddFractionsTest {
     public static class Fraction {
         private int numerator;
         private int denominator;
-        private int integerValue;
 
         public Fraction(int integerValue) {
-            this.integerValue = integerValue;
             this.numerator = integerValue;
             this.denominator = 1;
         }
 
         public Fraction(int numerator, int denominator) {
-            this.integerValue = numerator;
             this.numerator = numerator;
             this.denominator = denominator;
         }
@@ -74,7 +71,7 @@ public class AddFractionsTest {
         }
 
         public int intValue() {
-            return integerValue;
+            return numerator;
         }
 
         public int getNumerator() {
